61c5d4dffa0f77a2f70e385125f7b940b6b3d5d0
HuStmpHrrr <fdhzs2010@hotmail.com>
2019-01-25 18:39:30 -0500

    managing distribution

---
+++ build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+OUTPUT_DIR=
+INPUT_DIR=.
+
+while getopts i:o: OPT; do
+    case $OPT in
+        i) INPUT_DIR=$OPTARG;;
+        o) OUTPUT_DIR=$OPTARG;;
+        :)
+            echo "-$OPTARG needs an argument" >&2
+            exit 1;;
+        \?)
+            echo "unknown option -"$OPTARG >&2
+            exit 1;;
+    esac
+done
+
+if [[ $OUTPUT_DIR == "" ]]; then
+    echo "need an output folder! -o something" >&2
+    exit 1
+fi
+
+if [[ ! -d $INPUT_DIR ]]; then
+    echo "input folder $INPUT_DIR is not a folder! " >&2
+    exit 1
+fi
+
+mkdir -p "$OUTPUT_DIR"
+cp -r "$INPUT_DIR/static/"* "$OUTPUT_DIR/"
+python "$DIR/generate.py" "$DIR/CV.yaml" "$DIR/index.tmpl" -o "$OUTPUT_DIR/index.html"
---
+++ generate.py
@@ -0,0 +1,32 @@
+import argparse
+import yaml
+import os
+
+from jinja2 import Environment, FileSystemLoader
+
+parser = argparse.ArgumentParser(description="generate my personal site.")
+parser.add_argument("cv", type=str, help='A Yaml file representing the CV.')
+parser.add_argument('template', type=str,
+                    help='template file to generate Html from.')
+parser.add_argument('-o', '--output', type=str,
+                    help='final output file. stdout if not specified')
+
+
+def main():
+    result = parser.parse_args()
+    with open(result.cv) as fd:
+        cv = yaml.load(fd)
+
+    env = Environment(loader=FileSystemLoader(os.path.abspath(
+        os.path.join(result.template, os.path.pardir))))
+    tmpl = env.get_template(os.path.basename(result.template))
+
+    if result.output is None:
+        print tmpl.render(**cv).encode('UTF-8')
+    else:
+        with open(result.output, 'wb') as fd:
+            fd.write(tmpl.render(**cv).encode('UTF-8'))
+
+
+if __name__ == '__main__':
+    main()
